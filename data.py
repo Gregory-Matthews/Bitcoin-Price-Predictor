edb04c1519fa4abe18610e819970fe0bfbfc0c45
Gregory-Matthews <gm453@drexel.edu>
2017-11-17 14:10:40 -0500

    Add files via upload
    extract data from Quandl server

--- data.py
+++ data.py
@@ -1,33 +1,23 @@
 import quandl
-import tensorflow as tf
-import matplotlib.pyplot as mplot
 import numpy as np
 
-def main():
-    data = quandl.get("BCHAIN/MKPRU", returns="numpy")
-    m = data.shape[0] # number of training examples
-    print data.shape
+
+def get_data(feature, years):
+    # Retrieve Data
+    quandl.ApiConfig.api_key = 'xVPBqTzg3d4nXBob1JPz'
+
+    days = 365*years
+    data = quandl.get(feature, returns="numpy")
+    data = data[-days:]
+    m = int(data.shape[0])  # num of samples
+
+
+    train_Y = np.empty([m, 1])
+    train_X = np.arange(0, m)
 
     i = 0
-    Y = np.empty([m,1])
-    X = np.arange(0, m)
     for y in data:
-        Y[i] = y[1]
-        i+=1
-        
-    print X
-    print Y
+        train_Y[i] = y[1]
+        i += 1
 
-    #X = tf.placeholder("float")
-    #Y = tf.placeholder("float")
-
-    mplot.plot(X, Y, 'ro', label='Bitcoin Market Price')
-    mplot.legend()
-    mplot.show()
-
-
-if __name__ == "__main__":
-    main()
-
-
-
+    return train_X.reshape((m,)), train_Y.reshape((m,))
